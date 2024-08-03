2fc22375402b0fe3ace02641495ad642dbc299c9
Sergei Makhov <tdocsg00@gmail.com>
2024-08-03 14:37:30 +0300

    Added database and corrected logging

---
+++ src/main/java/org/example/ApartmentBot/model/User.java
@@ -0,0 +1,33 @@
+package org.example.ApartmentBot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Entity(name = "usersDataTable")
+public class User {
+    @Id
+    private Long chatId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String userName;
+
+    private Timestamp registeredAt;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "chatId=" + chatId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", registeredAt=" + registeredAt +
+                '}';
+    }
+}
